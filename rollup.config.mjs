7af55f2b6a7123901437005a98a4ae5ab6581e43
Rob Ellison <rob.ellison@airwalkconsulting.com>
2024-10-25 13:35:36 +0000

    fix rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,12 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import alias from '@rollup/plugin-alias';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   input: 'src/index.ts',
