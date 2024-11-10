d773c6eaee66c466274afa535c8434f24c75be4f
Rob Ellison <rob.ellison@airwalkconsulting.com>
2024-11-10 13:06:51 +0000

    feat: editor components

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 export * from '@/components/Cards';
 export * from '@/components/Blocks';
 export * from '@/components/Images';
+export * from '@/components/Editor';
 export * from '@/components/Layouts';
 export * from '@/components/Menus';
 export * from '@/components/ui';
