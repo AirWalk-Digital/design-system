0afff3df5b1376c8bfaa7effaeca94ce9e6be434
Fraser Steel <81355664+f-steel@users.noreply.github.com>
2024-11-15 11:47:49 +0000

    small change to trigger release

--- src/components/Cards/FeatureCard.tsx
+++ src/components/Cards/FeatureCard.tsx
@@ -1,30 +1,42 @@
-"use client";
+'use client';
 import DynamicIcon from '@/components/Images/DynamicIcon';
 import React from 'react';
 
 export type FeatureCardProps = {
-    title: string
-    description: string
-    iconName: string
-    iconStyle: 'solid' | 'regular' | 'light' | 'duotone' | 'kit'
-    color: 'green' | 'blue' | 'navy'
-  }
-  
- export const FeatureCard = ({ title, description, iconName, iconStyle, color }: FeatureCardProps) => {
+  title: string;
+  description: string;
+  iconName: string;
+  iconStyle: 'solid' | 'regular' | 'light' | 'duotone' | 'kit';
+  color: 'green' | 'blue' | 'navy' | 'red';
+};
 
-    const colorClasses = {
-      green: 'bg-green-500',
-      blue: 'bg-blue-400',
-      navy: 'bg-navy-700',
-    }
-  
-    return (
-      <div className={`rounded-3xl ${colorClasses[color]} p-6 flex flex-col items-center text-center`}>
-        <div className="w-12 h-12 " >
-        <DynamicIcon type={iconStyle} iconName={iconName} className="w-12 h-12 text-white" />
-        </div>
-        <h2 className="text-2xl font-bold text-white mb-2">{title}</h2>
-        <p className="text-white text-sm">{description}</p>
+export const FeatureCard = ({
+  title,
+  description,
+  iconName,
+  iconStyle,
+  color,
+}: FeatureCardProps) => {
+  const colorClasses = {
+    green: 'bg-green-500',
+    blue: 'bg-blue-400',
+    navy: 'bg-navy-700',
+    red: 'bg-red-500',
+  };
+
+  return (
+    <div
+      className={`rounded-3xl ${colorClasses[color]} p-6 flex flex-col items-center text-center`}
+    >
+      <div className="w-12 h-12 ">
+        <DynamicIcon
+          type={iconStyle}
+          iconName={iconName}
+          className="w-12 h-12 text-white"
+        />
       </div>
-    )
-  }
+      <h2 className="text-2xl font-bold text-white mb-2">{title}</h2>
+      <p className="text-white text-sm">{description}</p>
+    </div>
+  );
+};
