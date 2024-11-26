cf5de90b0f32993067a2832ea83799c3847b4eb4
Rob Ellison <rob.ellison@airwalkconsulting.com>
2024-11-26 12:29:53 +0000

    fix: revert preserveDirectives

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import preserveDirectives from 'rollup-plugin-preserve-directives';
 import alias from '@rollup/plugin-alias';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -42,7 +41,6 @@ export default {
       ],
     }),
     peerDepsExternal(),
-    preserveDirectives(),
     commonjs({
       include: 'node_modules/**', // Ensure that commonjs modules in node_modules are handled
     }),
